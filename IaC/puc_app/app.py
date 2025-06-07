a33d9ac4183a295e2a59cd1ce9dc149df48d3e12
Ravi Pellizzi <r.pellizzi09@gmail.com>
2025-06-07 13:49:39 -0300

    feat: inserindo cloudwatch

--- IaC/puc_app/app.py
+++ IaC/puc_app/app.py
@@ -2,30 +2,45 @@ from flask import Flask, request, render_template
 import boto3
 import os
 from werkzeug.utils import secure_filename
+import logging
+
+# Configura o logger para salvar em /tmp/app.log
+logging.basicConfig(
+    filename='/tmp/app.log',
+    level=logging.INFO,
+    format='%(asctime)s %(levelname)s: %(message)s'
+)
 
 app = Flask(__name__)
-bucket_name = "puc-914156456046-bucket"  
+bucket_name = "puc-914156456046-bucket"
 
 @app.route('/')
 def home():
+    logging.info("Página inicial acessada")
     return render_template('index.html')
 
 @app.route('/upload', methods=['POST'])
 def upload_csv():
     if 'file' not in request.files:
+        logging.warning("Tentativa de upload sem arquivo")
         return "Nenhum arquivo enviado", 400
     
     file = request.files['file']
     if file.filename == '':
+        logging.warning("Arquivo enviado sem nome")
         return "Nome de arquivo vazio", 400
 
     filename = secure_filename(file.filename)
 
-    # Upload para S3
-    s3 = boto3.client('s3')
-    s3.upload_fileobj(file, bucket_name, f"uploads/{filename}")
-
-    return f"Arquivo <strong>{filename}</strong> enviado com sucesso para o S3!"
+    try:
+        # Upload para S3
+        s3 = boto3.client('s3')
+        s3.upload_fileobj(file, bucket_name, f"uploads/{filename}")
+        logging.info(f"Arquivo {filename} enviado para S3 com sucesso")
+        return f"Arquivo <strong>{filename}</strong> enviado com sucesso para o S3!"
+    except Exception as e:
+        logging.error(f"Erro ao enviar arquivo para o S3: {e}")
+        return "Erro interno no servidor", 500
 
 if __name__ == "__main__":
     app.run(debug=True)
--- IaC/puc_iac_deploy/main.tf
+++ IaC/puc_iac_deploy/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "puc_bucket_flask" {
-  bucket = "puc-914156456046-bucket" 
+  bucket = "puc-914156456046-bucket"
 
   tags = {
     Name        = "puc Bucket"
@@ -14,95 +14,19 @@ resource "aws_s3_bucket" "puc_bucket_flask" {
     command = "${path.module}/upload_to_s3.sh"
   }
 
-  # Para destruir um S3, é preciso antes excluir os arquivos do S3. 
-  # rm é de remove e (--recursive) é para limpar de maneira recursiva o conteúdo do Bucket S3.  
-  # O command remove o conteúdo do S3 primeiro, e ai o terraform faz o trabalho dele com o destroy.
-
   provisioner "local-exec" {
     when    = destroy
     command = "aws s3 rm s3://puc-914156456046-bucket --recursive"
   }
 }
 
-resource "aws_instance" "puc_api" {
-
-  ami = "ami-0a0d9cf81c479446a"  
-
-  instance_type = "t2.micro"
-
-  iam_instance_profile = aws_iam_instance_profile.ec2_s3_profile.name
-
-  vpc_security_group_ids = [aws_security_group.puc_api_sg.id]
-
-  # Script de inicialização
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo yum update -y
-              sudo yum install -y python3 python3-pip awscli
-              sudo pip3 install flask boto3 gunicorn pandas "urllib3<2.0" "prefect<2.0"
-              sudo mkdir /puc_app
-              sudo aws s3 sync s3://puc-914156456046-bucket /puc_app
-              cd /puc_app
-              nohup gunicorn -w 4 -b 0.0.0.0:5000 app:app &
-            EOF
-
-  tags = {
-    Name = "pucFlaskApp"
-  }
-}
-
-resource "aws_security_group" "puc_api_sg" {
-  
-  name        = "puc_api_sg"
-  
-  description = "Security Group for Flask App in EC2"
-
-  # Essa é aporta padrão do http.
-
-  ingress {
-    description = "Inbound Rule 1"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # 500 é a porta que vai rodar a aplicação. 
-
-  ingress {
-    description = "Inbound Rule 2"
-    from_port   = 5000
-    to_port     = 5000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # A porta 22 é do ssh. Vamos acessar o terminar pelo navegador e para isso, precisa estar com a porta 22 aberta.
-
-  ingress {
-    description = "Inbound Rule 3"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # A regra de saíde é qualquer porta com o protocolo tcp para qualquer endereço na internet.
-
-  egress {
-    description = "Outbound Rule"
-    from_port = 0
-    to_port = 65535
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_cloudwatch_log_group" "app_logs" {
+  name              = "puc-app-log-group"
+  retention_in_days = 14
 }
 
 resource "aws_iam_role" "ec2_s3_access_role" {
-  
   name = "ec2_s3_access_role"
-
-  # 'Version = "2012-10-17"' é padrão da política de segurança da AWS.
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
@@ -113,32 +37,48 @@ resource "aws_iam_role" "ec2_s3_access_role" {
         Principal = {
           Service = "ec2.amazonaws.com"
         }
-      },
+      }
     ]
   })
 }
 
 resource "aws_iam_role_policy" "s3_access_policy" {
-  
   name = "s3_access_policy"
-  
   role = aws_iam_role.ec2_s3_access_role.id
 
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
       {
-        Action = [
-          "s3:GetObject",
-          "s3:PutObject",
-          "s3:ListBucket"
-        ],
+        Action = ["s3:GetObject", "s3:PutObject", "s3:ListBucket"],
         Effect = "Allow",
         Resource = [
           "${aws_s3_bucket.puc_bucket_flask.arn}/*",
           "${aws_s3_bucket.puc_bucket_flask.arn}"
         ]
-      },
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "cloudwatch_agent_policy" {
+  name = "cloudwatch_agent_policy"
+  role = aws_iam_role.ec2_s3_access_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "logs:DescribeLogStreams",
+          "cloudwatch:PutMetricData"
+        ],
+        Resource = "*"
+      }
     ]
   })
 }
@@ -148,4 +88,107 @@ resource "aws_iam_instance_profile" "ec2_s3_profile" {
   role = aws_iam_role.ec2_s3_access_role.name
 }
 
+resource "aws_security_group" "puc_api_sg" {
+  name        = "puc_api_sg"
+  description = "Security Group for Flask App in EC2"
 
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "App Port"
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "All traffic"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "puc_api" {
+  ami                    = "ami-0a0d9cf81c479446a"
+  instance_type          = "t2.micro"
+  iam_instance_profile   = aws_iam_instance_profile.ec2_s3_profile.name
+  vpc_security_group_ids = [aws_security_group.puc_api_sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y python3 python3-pip awscli amazon-cloudwatch-agent
+
+              pip3 install flask boto3 gunicorn pandas "urllib3<2.0" "prefect<2.0"
+
+              mkdir /puc_app
+              aws s3 sync s3://puc-914156456046-bucket /puc_app
+              cd /puc_app
+              nohup gunicorn -w 4 -b 0.0.0.0:5000 app:app &
+
+              cat <<EOT > /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
+              {
+                "agent": {
+                  "metrics_collection_interval": 60,
+                  "run_as_user": "root"
+                },
+                "logs": {
+                  "logs_collected": {
+                    "files": {
+                      "collect_list": [
+                        {
+                          "file_path": "/tmp/app.log",
+                          "log_group_name": "puc-app-log-group",
+                          "log_stream_name": "app.py",
+                          "timestamp_format": "%Y-%m-%d %H:%M:%S"
+                        }
+                      ]
+                    }
+                  }
+                },
+                "metrics": {
+                  "metrics_collected": {
+                    "cpu": {
+                      "measurement": ["usage_idle", "usage_user", "usage_system"],
+                      "totalcpu": true
+                    },
+                    "mem": {
+                      "measurement": ["mem_used_percent"]
+                    },
+                    "disk": {
+                      "measurement": ["used_percent"],
+                      "resources": ["*"]
+                    }
+                  }
+                }
+              }
+              EOT
+
+              /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+                -a fetch-config \
+                -m ec2 \
+                -c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json \
+                -s
+              EOF
+
+  tags = {
+    Name = "pucFlaskApp"
+  }
+}
